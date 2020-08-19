aef3e9682c83d606f2b45c5e77b44adf6ff2e065
Edward Boland <mr.eboland@gmail.com>
2020-08-19 14:50:42 -0400

    Updates

---
+++ precache-manifest.add7d3aba7297967928c1c6aaeae22d9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9723dddfc32aeab649040b8be3648d47",
+    "url": "/project2React/index.html"
+  },
+  {
+    "revision": "32b120678110cbd8cf7e",
+    "url": "/project2React/static/css/main.194ddebe.chunk.css"
+  },
+  {
+    "revision": "cde4bd3023d599a82cd3",
+    "url": "/project2React/static/js/2.db136f84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project2React/static/js/2.db136f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b120678110cbd8cf7e",
+    "url": "/project2React/static/js/main.36872adc.chunk.js"
+  },
+  {
+    "revision": "9da7ddaf262fe2d9c750",
+    "url": "/project2React/static/js/runtime-main.04ccc2ed.js"
+  },
+  {
+    "revision": "bfdf501cb993e6d9f9770110807482d4",
+    "url": "/project2React/static/media/blog-1.bfdf501c.jpeg"
+  },
+  {
+    "revision": "6e391c132d89a16824b9058fadfca57a",
+    "url": "/project2React/static/media/blog-3.6e391c13.jpeg"
+  },
+  {
+    "revision": "f17c164f31245dadc288d8ab2e8edd96",
+    "url": "/project2React/static/media/blog-4.f17c164f.jpeg"
+  },
+  {
+    "revision": "3cd69fa7e3a7a95717a8db2ad25cc0f7",
+    "url": "/project2React/static/media/blog-5.3cd69fa7.jpeg"
+  },
+  {
+    "revision": "3ffd8ca57ea97cb2fd8c5b87a0db3091",
+    "url": "/project2React/static/media/contact-1.3ffd8ca5.jpeg"
+  },
+  {
+    "revision": "90bdd1620309438f6a95749a7a617640",
+    "url": "/project2React/static/media/contact-2.90bdd162.jpeg"
+  },
+  {
+    "revision": "d0f752b2ef16c91b3108e9546c78de48",
+    "url": "/project2React/static/media/home-1.d0f752b2.jpeg"
+  },
+  {
+    "revision": "6ecc456341e98931bf6673db7116fb21",
+    "url": "/project2React/static/media/home-10.6ecc4563.jpeg"
+  },
+  {
+    "revision": "c1a5f05c2357e4f799f63dacbe735137",
+    "url": "/project2React/static/media/home-2.c1a5f05c.jpeg"
+  },
+  {
+    "revision": "0a0cf6a3f8bd9b988ad640d754b87a12",
+    "url": "/project2React/static/media/home-3.0a0cf6a3.jpeg"
+  },
+  {
+    "revision": "a35f5944708c8b72c1b06298763e5e07",
+    "url": "/project2React/static/media/home-4.a35f5944.jpeg"
+  },
+  {
+    "revision": "82e69f5299ffe83ad7b838e9a2ee38ab",
+    "url": "/project2React/static/media/home-5.82e69f52.jpeg"
+  },
+  {
+    "revision": "f93e5242bba2041a9a801c70ad040072",
+    "url": "/project2React/static/media/home-6.f93e5242.jpeg"
+  },
+  {
+    "revision": "04a849c1a2d5f19ce4f35e0901a77f66",
+    "url": "/project2React/static/media/home-7.04a849c1.jpeg"
+  },
+  {
+    "revision": "0a691809c8ece5d6c706116efebc9dd4",
+    "url": "/project2React/static/media/home-8.0a691809.jpeg"
+  },
+  {
+    "revision": "c12bb1da2a249cde95a794c76cd725d9",
+    "url": "/project2React/static/media/home-9.c12bb1da.jpeg"
+  },
+  {
+    "revision": "756cbe44fe47b9cebb5a6b645f7aceed",
+    "url": "/project2React/static/media/pattern.756cbe44.png"
+  }
+]);
